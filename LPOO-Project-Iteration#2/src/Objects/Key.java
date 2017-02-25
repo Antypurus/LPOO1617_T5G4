022c56a6a554dcde59151aa75778dd98953cf563
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-02-25 20:24:32 +0000

    map work done , and added a class to represent the dimensions of the map for easier constructor usage

--- LPOO-Project-Iteration#2/src/Objects/Key.java
+++ LPOO-Project-Iteration#2/src/Objects/Key.java
@@ -4,10 +4,26 @@ public class Key {
 
     private int xPos;
     private int yPos;
+    private boolean wasPicked=false;
 
     public Key(int x,int y){
         xPos=x;
         yPos=y;
     }
 
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public boolean isWasPicked() {
+        return wasPicked;
+    }
+
+    public void setWasPicked(boolean wasPicked) {
+        this.wasPicked = wasPicked;
+    }
 }
