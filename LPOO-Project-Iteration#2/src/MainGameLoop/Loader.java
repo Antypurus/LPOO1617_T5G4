5899e89cca28e24cbd4ee962f22ae679dc714fcd
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-02-26 18:39:00 +0000

    Map loader added

---
+++ LPOO-Project-Iteration#2/src/MainGameLoop/Loader.java
@@ -0,0 +1,13 @@
+package MainGameLoop;
+
+import Maps.MapDimension;
+
+public class Loader {
+
+    private String[][] map1=new String[10][10];
+    private MapDimension map1Dimension=new MapDimension(10,10);
+
+    private String[][] map2=new String[10][10];
+    private MapDimension map2Dimension=new MapDimension(10,10);
+
+}
--- LPOO-Project-Iteration#2/src/Maps/Map.java
+++ LPOO-Project-Iteration#2/src/Maps/Map.java
@@ -118,6 +118,7 @@ public class Map implements GameMap {
         }
 
         resetMap();
+
     }
 
     public boolean mapLogic(){
