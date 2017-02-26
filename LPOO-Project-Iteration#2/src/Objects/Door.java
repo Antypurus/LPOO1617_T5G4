7cbeb25601bd3587d4e482077e26931d8f742589
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-02-26 18:53:15 +0000

    added class that represents openable doors

---
+++ LPOO-Project-Iteration#2/src/Objects/Door.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class Door {
+    private int xPos;
+    private int yPos;
+    private boolean isOpean=false;
+    private String openRep="S";
+    private String closedRep="I";
+
+    public Door(int xPos,int yPos){
+
+    }
+}
