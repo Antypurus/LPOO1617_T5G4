8caf3ab9f0f17edc7bdaf30c828f78a2750dad21
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-02-27 23:52:51 +0000

    text input is now being managed and being transformed into information for the character movement

--- LPOO-Project-Iteration#2/src/inputs/TextInput.java
+++ LPOO-Project-Iteration#2/src/inputs/TextInput.java
@@ -20,5 +20,24 @@ public class TextInput {
         input=inputreader.next();
         return input;
     }
+    
+    public int getNextStep(){
+    	String check = read();
+    	
+    	if(check.equals("Up")||check.equals("up")||check.equals("w")){
+    		return 1;
+    	}
+    	if(check.equals("Down")||check.equals("down")||check.equals("s")){
+    		return 2;
+    	}
+    	if(check.equals("left")||check.equals("Left")||check.equals("a")){
+    		return 3;
+    	}
+    	if(check.equals("Right")||check.equals("right")||check.equals("d")){
+    		return 4;
+    	}
+    	
+    	return 0;
+    }
 
 }
