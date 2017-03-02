f8e1716715631bfd4d4f2b0cd4ba5fbf349dfd8e
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-03-02 10:20:41 +0000

    created two of the 3 possible guard strategies and their omplementation , now i just need to finish the last one and allow for randon selection of type of guard

--- LPOO-Project-Iteration#2/src/Characters/Guard.java
+++ LPOO-Project-Iteration#2/src/Characters/Guard.java
@@ -1,7 +1,13 @@
 package Characters;
 
+import java.util.Random;
+
 public class Guard extends Enemy {
 	private int guardMovStage = 0;
+	private String awakeRep = "G";
+	private String sleepRep = "g";
+	private boolean isSleep = false;
+	private boolean reverse = false;
 	
 	public Guard(String[][] map){
 		type = "Guard";
@@ -21,7 +27,7 @@ public class Guard extends Enemy {
 		this.map=map;
 	}
 	
-	private void moveGuard() {
+	private void rookieMoveGuard() {
 		if (guardMovStage == 0) {
 			xPos--;
 			guardMovStage = 1;
@@ -144,8 +150,136 @@ public class Guard extends Enemy {
 		}
 	}
 
+	private void reverseMovement(){
+		switch(guardMovStage){
+			case(0):
+				guardMovStage=23;
+				yPos++;
+				return;
+			case(1):
+				guardMovStage=0;
+				xPos++;
+				return;
+			case(2):
+				guardMovStage=1;
+				yPos--;
+				return ;
+			case(3):
+				guardMovStage=2;
+				yPos--;
+				return ;
+			case(4):
+				guardMovStage=3;
+				yPos--;
+				return ;
+			case(5):
+				guardMovStage=3;
+				yPos--;
+				return;
+			case(6):
+				guardMovStage=5;
+				xPos++;
+				return ;
+			case(7):
+				guardMovStage=6;
+				xPos++;
+				return ;
+			case(8):
+				guardMovStage=7;
+				xPos++;
+				return ;
+			case (9):
+				guardMovStage=8;
+				xPos++;
+				return ;
+			case(10):
+				guardMovStage=9;
+				xPos++;
+				return ;
+			case(11):
+				guardMovStage=10;
+				xPos++;
+				return ;
+			case(12):
+				guardMovStage=11;
+				yPos--;
+				return ;
+			case(13):
+				guardMovStage=12;
+				xPos--;
+				return ;
+			case(14):
+				guardMovStage=13;
+				xPos--;
+				return ;
+			case(15):
+				guardMovStage=14;
+				xPos--;
+				return ;
+			case(16):
+				guardMovStage=15;
+				xPos--;
+				return ;
+			case(17):
+				guardMovStage=16;
+				xPos--;
+				return ;
+			case(18):
+				guardMovStage=17;
+				xPos--;
+				return ;
+			case(19):
+				guardMovStage=18;
+				xPos--;
+				return;
+			case(20):
+				guardMovStage=19;
+				yPos++;
+				return;
+			case(21):
+				guardMovStage=20;
+				yPos++;
+				return;
+			case(22):
+				guardMovStage=21;
+				yPos++;
+				return;
+			case(23):
+				guardMovStage=22;
+				yPos++;
+				return;
+			default:return;
+		}
+	}
+
+	private void drunkenMoveGuard(){
+		if(isSleep){
+			isSleep=false;
+			representation=awakeRep;
+			Random generator = new Random();
+			int i=generator.nextInt(3);
+			if(i==1){
+				if(!reverse){
+				reverse=true;}else{
+					reverse=false;
+				}
+			}
+		}else{
+			if(reverse){
+			this.reverseMovement();
+			}else{
+			this.rookieMoveGuard();}
+			Random generator = new Random();
+			int test = generator.nextInt(3);
+			if(test==1){
+				this.isSleep=true;
+				representation=sleepRep;
+			}
+		}
+	}
+
 	public void move() {
-		moveGuard();// move calls the method specific to the guards movement
+		rookieMoveGuard();// move calls the method specific to the guards movement
 	}
 
 	public void attack() {
