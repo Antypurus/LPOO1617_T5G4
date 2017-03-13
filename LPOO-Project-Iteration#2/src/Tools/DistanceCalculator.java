411dc99df13dcb5d57827bdec35977051db0864b
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-03-13 01:47:20 +0000

    added bse distance calculator fucntionality

--- LPOO-Project-Iteration#2/src/Tools/DistanceCalculator.java
+++ LPOO-Project-Iteration#2/src/Tools/DistanceCalculator.java
@@ -7,8 +7,22 @@ import Characters.Hero;
 public class DistanceCalculator {
     public int xPos1,yPos1;
     public int xPos2,yPos2;
+    public double distance;
 
     public DistanceCalculator(Hero hero, Enemy enemie){
-        
+        this.xPos1=hero.getXPos();
+        this.yPos1=hero.getYPos();
+        this.xPos2=enemie.getXPos();
+        this.yPos2=enemie.getYPos();
+    }
+
+    private double calculateDistance(){
+        distance=Math.sqrt(Math.pow((xPos1-xPos2),2)+Math.pow((yPos1-yPos2),2));
+        return distance;
+    }
+
+    public double getDistance(){
+        this.calculateDistance();
+        return this.distance;
     }
 }
