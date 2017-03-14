c3e99fe39114cfe3b837fc6680a0219ae6956114
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-03-14 15:48:23 +0000

    Modified Component Names for Swing Window

--- LPOO-Project-Iteration#4/src/SWING_COMPONENTS/Main_Window.java
+++ LPOO-Project-Iteration#4/src/SWING_COMPONENTS/Main_Window.java
@@ -3,10 +3,20 @@ package SWING_COMPONENTS;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class Main_Window {
 
-	private JFrame frame;
+	private JFrame frmDungeonKepp;
+	private JTextField OggreCounter;
 
 	/**
 	 * Launch the application.
@@ -16,7 +26,7 @@ public class Main_Window {
 			public void run() {
 				try {
 					Main_Window window = new Main_Window();
-					window.frame.setVisible(true);
+					window.frmDungeonKepp.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +45,66 @@ public class Main_Window {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDungeonKepp = new JFrame();
+		frmDungeonKepp.setResizable(false);
+		frmDungeonKepp.setTitle("Dungeon Keep");
+		frmDungeonKepp.setBounds(100, 100, 532, 460);
+		frmDungeonKepp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDungeonKepp.getContentPane().setLayout(null);
+		
+		JLabel OggreCounterLabel = new JLabel("Number Of Oggres");
+		OggreCounterLabel.setBounds(10, 11, 98, 14);
+		frmDungeonKepp.getContentPane().add(OggreCounterLabel);
+		
+		OggreCounter = new JTextField();
+		OggreCounter.setBounds(106, 8, 86, 20);
+		frmDungeonKepp.getContentPane().add(OggreCounter);
+		OggreCounter.setColumns(10);
+		
+		JLabel GuardPersonalityLabel = new JLabel("Guard Personality");
+		GuardPersonalityLabel.setBounds(10, 36, 86, 14);
+		frmDungeonKepp.getContentPane().add(GuardPersonalityLabel);
+		
+		JComboBox GuardPersonalitySelector = new JComboBox();
+		GuardPersonalitySelector.setModel(new DefaultComboBoxModel(new String[] {"Novice", "Drunken", "Suspicious"}));
+		GuardPersonalitySelector.setBounds(106, 36, 86, 20);
+		frmDungeonKepp.getContentPane().add(GuardPersonalitySelector);
+		
+		JButton NewGameButton = new JButton("New Game");
+		NewGameButton.setBounds(403, 86, 89, 23);
+		frmDungeonKepp.getContentPane().add(NewGameButton);
+		
+		JButton ExitButton = new JButton("Exit");
+		ExitButton.setBounds(403, 373, 89, 23);
+		frmDungeonKepp.getContentPane().add(ExitButton);
+		
+		JTextArea GameDisplayArea = new JTextArea();
+		GameDisplayArea.setEditable(false);
+		GameDisplayArea.setBounds(10, 86, 320, 310);
+		frmDungeonKepp.getContentPane().add(GameDisplayArea);
+		
+		JButton MovementButtonUp = new JButton("Up");
+		MovementButtonUp.setEnabled(false);
+		MovementButtonUp.setBounds(389, 202, 89, 23);
+		frmDungeonKepp.getContentPane().add(MovementButtonUp);
+		
+		JButton MovementButtonLeft = new JButton("Left");
+		MovementButtonLeft.setEnabled(false);
+		MovementButtonLeft.setBounds(340, 236, 89, 23);
+		frmDungeonKepp.getContentPane().add(MovementButtonLeft);
+		
+		JButton MovementButtonRight = new JButton("Right");
+		MovementButtonRight.setEnabled(false);
+		MovementButtonRight.setBounds(437, 236, 89, 23);
+		frmDungeonKepp.getContentPane().add(MovementButtonRight);
+		
+		JButton MovementButtonDown = new JButton("Down");
+		MovementButtonDown.setEnabled(false);
+		MovementButtonDown.setBounds(389, 270, 89, 23);
+		frmDungeonKepp.getContentPane().add(MovementButtonDown);
+		
+		JLabel GameStatusMessage = new JLabel("Message");
+		GameStatusMessage.setBounds(10, 406, 320, 14);
+		frmDungeonKepp.getContentPane().add(GameStatusMessage);
 	}
-
 }
