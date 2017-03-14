c01db53aa363a081c678c2942ed687668a48cc43
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-03-14 16:23:14 +0000

    Initial Button Functionality Added

--- LPOO-Project-Iteration#4/src/SWING_COMPONENTS/Main_Window.java
+++ LPOO-Project-Iteration#4/src/SWING_COMPONENTS/Main_Window.java
@@ -12,10 +12,14 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Main_Window {
 
-	private JFrame frmDungeonKepp;
+	private JFrame frmDungeonKeep;
 	private JTextField OggreCounter;
 
 	/**
@@ -26,7 +30,7 @@ public class Main_Window {
 			public void run() {
 				try {
 					Main_Window window = new Main_Window();
-					window.frmDungeonKepp.setVisible(true);
+					window.frmDungeonKeep.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,66 +49,106 @@ public class Main_Window {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmDungeonKepp = new JFrame();
-		frmDungeonKepp.setResizable(false);
-		frmDungeonKepp.setTitle("Dungeon Keep");
-		frmDungeonKepp.setBounds(100, 100, 532, 460);
-		frmDungeonKepp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmDungeonKepp.getContentPane().setLayout(null);
+		frmDungeonKeep = new JFrame();
+		frmDungeonKeep.setResizable(false);
+		frmDungeonKeep.setTitle("Dungeon Keep");
+		frmDungeonKeep.setBounds(100, 100, 532, 460);
+		frmDungeonKeep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDungeonKeep.getContentPane().setLayout(null);
 		
 		JLabel OggreCounterLabel = new JLabel("Number Of Oggres");
-		OggreCounterLabel.setBounds(10, 11, 98, 14);
-		frmDungeonKepp.getContentPane().add(OggreCounterLabel);
+		OggreCounterLabel.setBounds(10, 11, 120, 14);
+		frmDungeonKeep.getContentPane().add(OggreCounterLabel);
 		
 		OggreCounter = new JTextField();
-		OggreCounter.setBounds(106, 8, 86, 20);
-		frmDungeonKepp.getContentPane().add(OggreCounter);
+		OggreCounter.setBounds(134, 8, 86, 20);
+		frmDungeonKeep.getContentPane().add(OggreCounter);
 		OggreCounter.setColumns(10);
 		
 		JLabel GuardPersonalityLabel = new JLabel("Guard Personality");
-		GuardPersonalityLabel.setBounds(10, 36, 86, 14);
-		frmDungeonKepp.getContentPane().add(GuardPersonalityLabel);
+		GuardPersonalityLabel.setBounds(10, 36, 120, 14);
+		frmDungeonKeep.getContentPane().add(GuardPersonalityLabel);
 		
 		JComboBox GuardPersonalitySelector = new JComboBox();
 		GuardPersonalitySelector.setModel(new DefaultComboBoxModel(new String[] {"Novice", "Drunken", "Suspicious"}));
-		GuardPersonalitySelector.setBounds(106, 36, 86, 20);
-		frmDungeonKepp.getContentPane().add(GuardPersonalitySelector);
+		GuardPersonalitySelector.setBounds(134, 33, 86, 20);
+		frmDungeonKeep.getContentPane().add(GuardPersonalitySelector);
 		
-		JButton NewGameButton = new JButton("New Game");
-		NewGameButton.setBounds(403, 86, 89, 23);
-		frmDungeonKepp.getContentPane().add(NewGameButton);
-		
-		JButton ExitButton = new JButton("Exit");
-		ExitButton.setBounds(403, 373, 89, 23);
-		frmDungeonKepp.getContentPane().add(ExitButton);
 		
 		JTextArea GameDisplayArea = new JTextArea();
 		GameDisplayArea.setEditable(false);
 		GameDisplayArea.setBounds(10, 86, 320, 310);
-		frmDungeonKepp.getContentPane().add(GameDisplayArea);
+		frmDungeonKeep.getContentPane().add(GameDisplayArea);
 		
 		JButton MovementButtonUp = new JButton("Up");
 		MovementButtonUp.setEnabled(false);
 		MovementButtonUp.setBounds(389, 202, 89, 23);
-		frmDungeonKepp.getContentPane().add(MovementButtonUp);
+		frmDungeonKeep.getContentPane().add(MovementButtonUp);
 		
 		JButton MovementButtonLeft = new JButton("Left");
 		MovementButtonLeft.setEnabled(false);
 		MovementButtonLeft.setBounds(340, 236, 89, 23);
-		frmDungeonKepp.getContentPane().add(MovementButtonLeft);
+		frmDungeonKeep.getContentPane().add(MovementButtonLeft);
 		
 		JButton MovementButtonRight = new JButton("Right");
 		MovementButtonRight.setEnabled(false);
 		MovementButtonRight.setBounds(437, 236, 89, 23);
-		frmDungeonKepp.getContentPane().add(MovementButtonRight);
+		frmDungeonKeep.getContentPane().add(MovementButtonRight);
 		
 		JButton MovementButtonDown = new JButton("Down");
 		MovementButtonDown.setEnabled(false);
 		MovementButtonDown.setBounds(389, 270, 89, 23);
-		frmDungeonKepp.getContentPane().add(MovementButtonDown);
+		frmDungeonKeep.getContentPane().add(MovementButtonDown);
 		
-		JLabel GameStatusMessage = new JLabel("Message");
+		JLabel GameStatusMessage = new JLabel("");
 		GameStatusMessage.setBounds(10, 406, 320, 14);
-		frmDungeonKepp.getContentPane().add(GameStatusMessage);
+		frmDungeonKeep.getContentPane().add(GameStatusMessage);
+		
+		JButton NewGameButton = new JButton("New Game");
+		NewGameButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				GameStatusMessage.setText("Start A New Game");
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				GameStatusMessage.setText("");
+			}
+		});
+		
+		NewGameButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {	
+				
+				MovementButtonUp.setEnabled(true);
+				MovementButtonDown.setEnabled(true);
+				MovementButtonRight.setEnabled(true);
+				MovementButtonLeft.setEnabled(true);
+				
+				GameStatusMessage.setText("New Game Started");
+				
+			}
+		});
+		
+		NewGameButton.setBounds(389, 87, 89, 23);
+		frmDungeonKeep.getContentPane().add(NewGameButton);
+		
+		JButton ExitButton = new JButton("Exit");
+		ExitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				GameStatusMessage.setText("Exit Game");
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				GameStatusMessage.setText("");
+			}
+		});
+		ExitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		ExitButton.setBounds(389, 373, 89, 23);
+		frmDungeonKeep.getContentPane().add(ExitButton);
 	}
 }
