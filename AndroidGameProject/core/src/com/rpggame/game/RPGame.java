4e89c8706e9ac626d3319702aa5cd2c3e94f9a4c
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-04-05 11:36:11 +0100

    bfdsgdf

--- AndroidGameProject/core/src/com/rpggame/game/RPGame.java
+++ AndroidGameProject/core/src/com/rpggame/game/RPGame.java
@@ -23,7 +23,7 @@ public class RPGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img,0,0,0,0,2160,1440,1,1,0,0,0,2160,1440,false,false);
+		batch.draw(img,0,0);
 		batch.end();
 	}
 	
