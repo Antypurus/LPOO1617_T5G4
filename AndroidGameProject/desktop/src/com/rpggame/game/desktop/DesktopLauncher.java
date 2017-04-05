892e4f4f7a8e9d4d6d1370a13cabf8623a98be7c
Tiago José Sousa Magalhães <crakylps@gmail.com>
2017-04-05 10:54:29 +0100

    Commit of the android project
    Android project ready to run on android studio has been committed and should just need to be imported

---
+++ AndroidGameProject/desktop/src/com/rpggame/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rpggame.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rpggame.game.RPGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RPGame(), config);
+	}
+}
