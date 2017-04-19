6f1f996ced6879267ec065d459c2a29374c2f5e8
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-04-19 12:04:02 +0100

    added fps counter to window tittle

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -15,13 +16,19 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
-
+	double fps;
+	int x = 0;
+	int y = 0;
 	@Override
 	public void render () {
+		fps = Gdx.graphics.getFramesPerSecond();
+		Gdx.graphics.setTitle("RPGame FPS:"+fps);
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
+		x++;
+		y++;
 		batch.end();
 	}
 	
