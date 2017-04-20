06ff2c657f2877ff58b5d34ea9b6f1a893c4c6f0
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-04-20 15:48:06 +0100

    more testing

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.FPSLogger;
@@ -32,43 +33,65 @@ public class MyGdxGame extends ApplicationAdapter {
 		cursor = new Texture("CR_Cursor.png");
 		spr = new Sprite(img);
 		spr.setScale(0.15f);
-		Gdx.input.setCursorCatched(true);
+		//Gdx.input.setCursorCatched(true);
 		x = Gdx.graphics.getWidth()/2;
 		y = Gdx.graphics.getHeight()/2;
+		spr.setOrigin(0,0);
+		spr.setX(x);
+		spr.setY(y);
 	}
 	double fps;
 
 	@Override
 	public void render () {
+		Color cor = new Color(1,0,0,1);
 		fps = Gdx.graphics.getFramesPerSecond();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.disableBlending();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		spr.setPosition(x-15,y-15);
+		batch.enableBlending();
 		spr.draw(batch);
+		x = (int)spr.getX();
+		y = (int)spr.getY();
 		batch.draw(cursor,Gdx.input.getX(),Gdx.graphics.getHeight()-(Gdx.input.getY()+30),30,30);
 		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
 			y++;
+			spr.setY(y);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 			y--;
+			spr.setY(y);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 			x++;
+			spr.setX(x);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 			x--;
+			spr.setX(x);
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
 			Gdx.app.exit();
 		}
+		int cX = Gdx.input.getX();
+		int cY = Gdx.graphics.getHeight()-Gdx.input.getY();
+		float wd = spr.getScaleX()*spr.getWidth();
+		float hg = spr.getHeight()*spr.getScaleY();
+
+		if(cX>=x&&cX<=(x+wd)){
+			if(cY>=y&&cY<=(y+hg)){
+				spr.setColor(cor);
+			}
+		}
 		batch.end();
 		int b = batch.renderCalls;
-		Gdx.graphics.setTitle("RPGame FPS:"+fps+" Draw Calls This Frame:"+b);
+		Gdx.graphics.setTitle("RPGame FPS:"+fps+" Draw Calls This Frame:"+b+" Cursor Position("+cX+","+cY+") Target Height:" + wd + " Target Width:"+hg + " Target Position("+x+","+y+") X Region ["+x+","+(x+wd)+"]"
+		+ "Y Region ["+y+","+(y+hg)+"]");
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
