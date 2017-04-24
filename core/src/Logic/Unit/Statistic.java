861b3655efa531705956d178a8e6bed3256ca36a
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-04-24 14:25:52 +0100

    tentative changes and prototyping of the delta and buff system

--- core/src/Logic/Unit/Statistic.java
+++ core/src/Logic/Unit/Statistic.java
@@ -17,6 +17,26 @@ public class Statistic {
         Deltas.add(delta);
     }
 
+    public int findDelta(double delta){
+        for(int i=0;i<this.Deltas.size();i++){
+            if(this.Deltas.get(i)==delta){
+                return i;//value found
+            }
+        }
+        return -1;//value not found
+    }
+
+    public boolean removeDelta(double delta){
+        int deltaPos = this.findDelta(delta);
+        if(deltaPos==-1){
+            System.out.println("Delta Value Not Found");
+            return false;//value not found
+        }else{
+            this.Deltas.remove(deltaPos);
+        }
+        return false;
+    }
+
     public double update(){
         this.EffectiveValue = 0;
         this.EffectiveValue+=this.BaseValue;
