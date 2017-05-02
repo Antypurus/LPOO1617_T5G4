eb25ac23f7f762ff683e4a290409b372ea4cf658
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-05-03 00:03:45 +0100

    kinda got an animation working

--- core/src/com/mygdx/game/GraphicSystem/Character.java
+++ core/src/com/mygdx/game/GraphicSystem/Character.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.GraphicSystem;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -9,7 +10,7 @@ import Logic.Unit.Unit;
 
 public class Character {
     private Unit unit=null;
-    private Texture sprite=null;
+    private Sprite spr=null;
 
     private Texture interm = null;
     private TextureRegion[][] texts = null;
@@ -24,21 +25,24 @@ public class Character {
 
     private orientation ori = orientation.DOWN;
 
+    private float currKeyframe;
+
     public Character(){
         interm = new Texture("characters.png");
         texts = TextureRegion.split(interm,interm.getWidth()/12,interm.getHeight()/8);
         textures = new TextureRegion[3];
-        System.arraycopy(texts[0],0,textures,0,3);
-        sprite = textures[0].getTexture();
-        {
-            DownAnimation = new Animation<TextureRegion>(0.25f, textures);
-            sprite = DownAnimation.getKeyFrame(0f).getTexture();
-        }
+        System.arraycopy(texts[0],3,textures,0,3);
+        spr = new Sprite(textures[2]);
+        DownAnimation = new Animation<TextureRegion>(0.30f, textures);
+        DownAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        currKeyframe = 0;
         unit = new Unit("test",1,1,1,1,1);
     }
 
-    public Texture getSprite(){
-        return this.sprite;
+    public Sprite getSprite(){
+        spr.setRegion(DownAnimation.getKeyFrame(currKeyframe));
+        currKeyframe+= Gdx.graphics.getDeltaTime();
+        return this.spr;
     }
 
     public Unit getUnit(){
