28e974a6b4188c88f8489045672f8deb0a852236
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-05-02 22:46:27 +0100

    continued handler work

---
+++ core/src/com/mygdx/game/InputHandler/GameHandler.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.InputHandler;
+
+import com.mygdx.game.MyGdxGame;
+
+public class GameHandler extends InputHandler{
+
+    public GameHandler(MyGdxGame game){
+        super(game);
+    }
+
+    public void update(){
+
+    }
+
+}
---
+++ core/src/com/mygdx/game/InputHandler/MenuHandler.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.InputHandler;
+
+import com.mygdx.game.MyGdxGame;
+
+public class MenuHandler extends InputHandler{
+
+    public MenuHandler(MyGdxGame game){
+        super(game);
+    }
+
+    public void update(){
+
+    }
+
+}
