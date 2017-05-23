816624043199ba0d7bfe70033fe0203dbeb06087
Tiago Magalhães <up201607931@fe.up.pt>
2017-05-23 14:42:08 +0100

    started restructure of ability system

--- core/src/main/Logic/Abilities/Ability.java
+++ core/src/main/Logic/Abilities/Ability.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 
 import main.Logic.Unit.Unit;
 
-public abstract class Ability {
-
-    public String name = null;
-
-    public main.Logic.ElementSystem.Element.type AbilityType = null;
-    public main.Logic.ElementSystem.Element.DamageType type = null;
-    public main.Logic.ElementSystem.Element.DamageElement damageElement = null;
-
-    public double HitChange ;
-
-    public double value;
-    public int effectRadius;
-    public int targetRange;
-
-    public ArrayList<main.Logic.ElementSystem.Element.type> Traits = new ArrayList<main.Logic.ElementSystem.Element.type>();
+public interface Ability {
 
     public abstract void AffectTarget(Unit target);
+    public abstract boolean canHitTarget(Unit target);
+    public abstract double getBaseDamage();
+    public abstract double getRange();
+    public abstract boolean isElemental();
+    public abstract main.Logic.ElementSystem.Element.type getType();
+    public abstract main.Logic.ElementSystem.Element.DamageElement getDamageElement();
+    public abstract main.Logic.ElementSystem.Element.DamageType getDamageType();
+    public abstract String getName();
+    public abstract ArrayList<main.Logic.ElementSystem.Element.type> getTraits();
+    public abstract Unit getOwner();
 }
--- core/src/main/Logic/Abilities/Spells/Fireball.java
+++ core/src/main/Logic/Abilities/Spells/Fireball.java
@@ -4,25 +4,18 @@ import main.Logic.Abilities.Ability;
 import main.Logic.Unit.Stats.Inteligence;
 import main.Logic.Unit.Unit;
 
-public class Fireball extends Ability{
+public class Fireball{
 
     private final double Damage = 100;
     private final int AOE = 0;
     private final double Chance = 100;
     private final int range  = 10;
 
-    public Fireball(){
-        this.value = Damage;
-        this.effectRadius = AOE;
-        this.name = "FireBall";
-        this.targetRange = range;
+    public Fireball(Unit owner){
+
     }
 
     public void AffectTarget(Unit target){
-
-    }
-
-    public void setScalingStat(Inteligence stat){
 
     }
 
