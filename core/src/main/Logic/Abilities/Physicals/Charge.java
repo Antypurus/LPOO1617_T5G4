b959678c754cf543be04764c5ad14d5dcffc7585
Tiago Magalhães <up201607931@fe.up.pt>
2017-05-23 14:57:33 +0100

    more work on ability system

--- core/src/main/Logic/Abilities/Physicals/Charge.java
+++ core/src/main/Logic/Abilities/Physicals/Charge.java
@@ -1,4 +1,54 @@
 package main.Logic.Abilities.Physicals;
 
-public class Charge {
+import java.util.ArrayList;
+
+import main.Logic.Abilities.Ability;
+import main.Logic.ElementSystem.Element;
+import main.Logic.Unit.Unit;
+
+public class Charge implements Ability {
+
+    public void AffectTarget(Unit target){
+
+    }
+
+    public boolean canHitTarget(Unit target){
+        return false;
+    }
+
+    public double getBaseDamage(){
+        return 0;
+    }
+
+    public double getRange(){
+        return 0;
+    }
+
+    public boolean isElemental() {
+        return false;
+    }
+
+    public main.Logic.ElementSystem.Element.type getType(){
+        return null;
+    }
+
+    public main.Logic.ElementSystem.Element.DamageElement getDamageElement(){
+        return null;
+    }
+
+    public main.Logic.ElementSystem.Element.DamageType getDamageType(){
+        return null;
+    }
+
+    public String getName(){
+        return null;
+    }
+
+    public ArrayList<Element.type> getTraits(){
+        return null;
+    }
+
+    public Unit getOwner(){
+        return null;
+    }
 }
--- core/src/main/Logic/Abilities/Spells/Fireball.java
+++ core/src/main/Logic/Abilities/Spells/Fireball.java
@@ -1,22 +1,67 @@
 package main.Logic.Abilities.Spells;
 
+import java.util.ArrayList;
+
 import main.Logic.Abilities.Ability;
+import main.Logic.ElementSystem.Element;
 import main.Logic.Unit.Stats.Inteligence;
 import main.Logic.Unit.Unit;
 
-public class Fireball{
+public class Fireball implements Ability{
 
-    private final double Damage = 100;
-    private final int AOE = 0;
-    private final double Chance = 100;
-    private final int range  = 10;
+    private  double Damage = 100;
+    private  int AOE = 0;
+    private  double Chance = 100;
+    private  int range  = 10;
+
+
 
     public Fireball(Unit owner){
 
     }
-
+    
     public void AffectTarget(Unit target){
 
+    }
+
+    public boolean canHitTarget(Unit target){
+        return false;
+    }
+
+    public double getBaseDamage(){
+        return 0;
+    }
+
+    public double getRange(){
+        return 0;
+    }
+
+    public boolean isElemental(){
+        return false;
+    }
+
+    public main.Logic.ElementSystem.Element.type getType(){
+        return null;
+    }
+
+    public main.Logic.ElementSystem.Element.DamageElement getDamageElement(){
+        return null;
+    }
+
+    public main.Logic.ElementSystem.Element.DamageType getDamageType(){
+        return null;
+    }
+
+    public String getName(){
+        return null;
+    }
+
+    public ArrayList<Element.type> getTraits(){
+        return null;
+    }
+
+    public Unit getOwner(){
+        return null;
     }
 
 }
