7af67ed5520c7ac26970f4bcae8e44670613731f
Diogo <up201505472@fe.up.pt>
2017-05-24 17:46:43 +0100

    DifficultyStage

--- core/src/main/game/States/DifficultyStage.java
+++ core/src/main/game/States/DifficultyStage.java
@@ -1,5 +1,6 @@
 package game.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DifficultyStage extends State
 {
+    private Texture background;
     private Texture easyBtn;
     private Texture hardBtn;
 
@@ -16,25 +18,50 @@ public class DifficultyStage extends State
         super(gsm);
         easyBtn = new Texture("easy.png");
         hardBtn = new Texture("hard.png");
+        background = new Texture("black.png");
     }
 
     @Override
     protected void handleInput() {
+        if(Gdx.input.justTouched())
+        {
+            if (Gdx.input.getX() >= ((this.screenWidth / 2) - (easyBtn.getWidth() / 2))
+                    && Gdx.input.getX() <= ((screenWidth / 2) + (easyBtn.getWidth() / 2))
+                    && (screenHeight - Gdx.input.getY()) >= ((400))
+                    && (screenHeight - Gdx.input.getY()) <= ((400) + (easyBtn.getHeight())))
+            {
+                gsm.set(new PlayState(gsm));
+            }
 
+            else if (Gdx.input.getX() >= ((screenWidth / 2) - (hardBtn.getWidth() / 2))
+                    && Gdx.input.getX() <= ((screenWidth / 2) + (hardBtn.getWidth() / 2))
+                    && (screenHeight - Gdx.input.getY()) >= ((250))
+                    && (screenHeight - Gdx.input.getY()) <= ((250) + (hardBtn.getHeight())))
+            {
+                gsm.set(new PlayState(gsm));
+            }
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
     }
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0);
+        sb.draw(easyBtn, (screenWidth/2)- (easyBtn.getWidth()/2), 400);
+        sb.draw(hardBtn, (screenWidth/2)- (hardBtn.getWidth()/2), 250);
+        sb.end();
     }
 
     @Override
-    public void dispose() {
-
+    public void dispose()
+    {
+        hardBtn.dispose();
+        easyBtn.dispose();
+        background.dispose();
     }
 }
