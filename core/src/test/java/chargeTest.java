aa26f8aa41889a292f1b216b008aeaefc494a9eb
Tiago Magalhães <up201607931@fe.up.pt>
2017-05-26 15:11:13 +0100

    more work on the charge test

--- core/src/test/java/chargeTest.java
+++ core/src/test/java/chargeTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import main.Logic.Abilities.Ability;
 import main.Logic.Abilities.Physicals.Charge;
 import main.Logic.ElementSystem.Element;
+import main.Logic.Map.Map;
 import main.Logic.Unit.Unit;
 
 import static org.junit.Assert.assertEquals;
@@ -42,5 +43,46 @@ public class chargeTest {
         assertEquals(Element.type.DAMAGE,testCharge.getType());
         assertEquals(Element.DamageType.Physical,testCharge.getDamageType());
         assertEquals(null,testCharge.getDamageElement());
+    }
+
+    @Test
+    public void canHitInRange(){
+        Unit testUnit = new Unit("Test Unit",5,2,5,6,2);
+        Unit testUnit2 = new Unit("Test Unit",5,2,5,6,2);
+        Map testMap = new Map("Test Map",30,30);
+        Ability testCharge = new Charge(testUnit);
+
+        testUnit.setPosition(testMap.getCell(10,10));
+        testUnit2.setPosition(testMap.getCell(11,10));
+
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit2.setPosition(testMap.getCell(20,10));
+        testUnit.setPosition(testMap.getCell(5,10));
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit2.setPosition(testMap.getCell(5,11));
+        testUnit.setPosition(testMap.getCell(5,10));
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit2.setPosition(testMap.getCell(5,25));
+        testUnit.setPosition(testMap.getCell(5,10));
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit.setPosition(testMap.getCell(20,10));
+        testUnit2.setPosition(testMap.getCell(5,10));
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit2.setPosition(testMap.getCell(19,10));
+        testUnit.setPosition(testMap.getCell(20,10));
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit2.setPosition(testMap.getCell(25,28));
+        testUnit.setPosition(testMap.getCell(25,29));
+        assertTrue(testCharge.canHitTarget(testUnit2));
+
+        testUnit2.setPosition(testMap.getCell(20,14));
+        testUnit.setPosition(testMap.getCell(20,29));
+        assertTrue(testCharge.canHitTarget(testUnit2));
     }
 }
