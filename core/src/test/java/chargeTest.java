022b6f3676e75015985d1dd69cf4071d08a32af9
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-05-26 14:14:49 +0100

    started work on testing the charge ability

--- core/src/test/java/chargeTest.java
+++ core/src/test/java/chargeTest.java
@@ -1,8 +1,29 @@
 import org.junit.Test;
 
+import main.Logic.Abilities.Ability;
+import main.Logic.Abilities.Physicals.Charge;
+import main.Logic.Unit.Unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class chargeTest {
     @Test
     public void cretionTest(){
+        Unit testUnit = new Unit("Test Unit",5,2,5,6,2);
+        Ability testCharge = new Charge(testUnit);
 
+        assertEquals(1,testUnit.getAbilities().size());
+        assertEquals(testUnit,testCharge.getOwner());
+        assertEquals("Charge",testCharge.getName());
+        assertTrue(testUnit.getAbilities().get(0).equals(testCharge));
+        assertEquals(testUnit.StatFind("STR"),testCharge.getScalingStat());
+    }
+
+    @Test
+    public void testChargeStat(){
+        Unit testUnit = new Unit("Test Unit",5,2,5,6,2);
+        Ability testCharge = new Charge(testUnit);
+        
     }
 }
