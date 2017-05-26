d676a83ae19328f4256bb3a791f6697cd9f602cf
Tiago Magalhães <up201607931@fe.up.pt>
2017-05-26 14:37:21 +0100

    more work on the charge test

--- core/src/test/java/chargeTest.java
+++ core/src/test/java/chargeTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import main.Logic.Abilities.Ability;
 import main.Logic.Abilities.Physicals.Charge;
+import main.Logic.ElementSystem.Element;
 import main.Logic.Unit.Unit;
 
 import static org.junit.Assert.assertEquals;
@@ -24,6 +25,22 @@ public class chargeTest {
     public void testChargeStat(){
         Unit testUnit = new Unit("Test Unit",5,2,5,6,2);
         Ability testCharge = new Charge(testUnit);
-        
+
+        assertEquals(20,testCharge.getBaseDamage(),0.01);
+        assertEquals(60,testCharge.getHitChance(),0.01);
+        assertEquals(15,testCharge.getRange());
+        assertEquals(0,testCharge.getAOE());
+        assertEquals(0,testCharge.getManaCost(),0.01);
+        assertEquals(null,testCharge.getTraits());
+    }
+
+    @Test
+    public void testChargeEnums(){
+        Unit testUnit = new Unit("Test Unit",5,2,5,6,2);
+        Ability testCharge = new Charge(testUnit);
+
+        assertEquals(Element.type.DAMAGE,testCharge.getType());
+        assertEquals(Element.DamageType.Physical,testCharge.getDamageType());
+        assertEquals(null,testCharge.getDamageElement());
     }
 }
