c36001c36ae5322a8a964d158fdb37d8dd3e9a9b
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-05-30 20:49:29 +0100

    continued work on the turn system

--- core/src/main/Logic/GameController.java
+++ core/src/main/Logic/GameController.java
@@ -3,6 +3,7 @@ package Logic;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
+import main.Logic.Map.Map;
 import main.Logic.Unit.Unit;
 
 public class GameController {
@@ -11,10 +12,11 @@ public class GameController {
     private PriorityQueue<Unit>units = new PriorityQueue<Unit>();
     private Difficulty.DifficultyStage currentDifficulty = null;
     private int numberOfChars = 0;
-    private int selectedChar = 0;
     private Unit currentChar = null;
+    private Map gameMap = null;
 
-    public GameController(Unit[] allies,Unit[] enemies,Difficulty.DifficultyStage difficultyStage){
+    public GameController(Unit[] allies,Unit[] enemies,Difficulty.DifficultyStage difficultyStage,Map map){
+        this.gameMap = map;
         this.currentDifficulty = difficultyStage;
         for(int i=0;i<allies.length;++i){
             allies[i].setIsAlly(true);
@@ -29,5 +31,71 @@ public class GameController {
             numberOfChars++;
         }
         this.currentChar = this.units.peek();
+    }
+
+    public Unit getCurrentChar(){
+        return this.currentChar;
+    }
+
+    public Map getGameMap(){
+        return this.gameMap;
+    }
+
+    public int getNumberOfChars(){
+        return this.numberOfChars;
+    }
+
+    public Difficulty.DifficultyStage getCurrentDifficulty(){
+        return this.currentDifficulty;
+    }
+
+    private boolean rebuildUnitQueue(){
+        for(int i=0;i<this.allies.size();++i){
+            if(!this.allies.get(i).isDead()){
+                this.units.add(this.allies.get(i));
+            }
+        }
+        for(int i=0;i<this.enemies.size();++i){
+            if(!this.enemies.get(i).isDead()){
+                this.units.add(this.enemies.get(i));
+            }
+        }
+        if(this.units.size()==0||this.units.size()==1){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasWon(){
+        if(this.allies.size()!=0){
+            if(this.enemies.size()==0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasLost(){
+        if(this.allies.size()==0){
+            if(this.enemies.size()!=0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void startNextTurn(){
+        this.currentChar = this.units.peek();
+        if(this.currentChar!=null){
+            this.currentChar.beginTurn();
+        }
+        //if current char is ai do ai things and end turn else return so that player can do his shit.
+    }
+
+    public void endTurn(){
+        this.units.remove();
+        if(this.units.size()==0){
+            this.rebuildUnitQueue();
+        }
     }
 }
