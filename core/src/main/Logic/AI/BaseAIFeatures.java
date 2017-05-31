6b7ca32cdbc90b041f75c13549094ae59e2d6bdf
Tiago Magalhães <up201607931@fe.up.pt>
2017-06-01 00:49:38 +0100

    ai features

---
+++ core/src/main/Logic/AI/BaseAIFeatures.java
@@ -0,0 +1,32 @@
+package Logic.AI;
+
+import main.Logic.ElementSystem.Element;
+import main.Logic.Unit.Unit;
+
+public class BaseAIFeatures {
+
+    public int numberOfOffensivAbilitiesAvailableToUse(Unit owner){
+        int ret = 0;
+        for(int i=0;i<owner.getAbilities().size();++i){
+            if(owner.getAbilities().get(i).getType().equals(Element.type.DAMAGE)){
+                if(owner.getAbilities().get(i).canUse()){
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+
+    public int numberOfHealingAbilitiesAvailableToUse(Unit owner){
+        int ret = 0;
+        for(int i=0;i<owner.getAbilities().size();++i){
+            if(owner.getAbilities().get(i).getType().equals(Element.type.HEAL)){
+                if(owner.getAbilities().get(i).canUse()){
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+
+}
