073856e334a766a581705d3cc0eff2e1ed2164eb
Tiago Magalhães <up201607931@fe.up.pt>
2017-06-02 22:05:59 +0100

    dps offensive behavior done

--- core/src/main/Logic/AI/DPSAI/DPSAI.java
+++ core/src/main/Logic/AI/DPSAI/DPSAI.java
@@ -1,16 +1,71 @@
 package Logic.AI.DPSAI;
 
+import java.util.ArrayList;
+
 import Logic.AI.BaseAIFeatures;
 import Logic.AI.BaseAi;
 import Logic.Difficulty;
 import Logic.GameController;
+import main.Logic.Abilities.Ability;
+import main.Logic.Map.Cell;
 import main.Logic.Unit.Unit;
 
 public class DPSAI extends BaseAIFeatures implements BaseAi{
+    private Unit DPSAIUnit = null;
+    private GameController CurrentGame = null;
+
+    public DPSAI(Unit owner,GameController game){
+        this.DPSAIUnit = owner;
+        this.CurrentGame = game;
+    }
+
+    private void easyOffensiveBehavior(){
+        Unit target = this.determineClosestEnemy(this.CurrentGame,this.DPSAIUnit);
+        if(this.DPSAIUnit.getMP()>0){
+            Ability abl = this.abilityWithMostDamage(this.DPSAIUnit);
+            ArrayList<Cell> cells = null;
+            this.cellsFromWhereCanHitTargetWithAbility(abl,target,cells,this.DPSAIUnit);
+            if(cells.size()==0){//cant thit
+                if(this.DPSAIUnit.getMP()-abl.getManaCost()<0){//cant hit because of lack of mana
+                    for(int i=0;i<this.DPSAIUnit.getAbilities().size();++i){
+                        abl = this.DPSAIUnit.getAbilities().get(i);
+                        cells.clear();
+                        this.cellsFromWhereCanHitTargetWithAbility(abl,target,cells,this.DPSAIUnit);
+                        if(cells.size()!=0){
+                            Cell cell = this.cellAtSmalestDistance(cells,this.DPSAIUnit);
+                            this.DPSAIUnit.moveToCell(cell);
+                            abl.AffectTarget(target);
+                            return;
+                        }
+                    }
+                }else{//cant hit because of distance
+                    Cell cell = this.cellThatGetsClosestToTarget(this.DPSAIUnit,target);
+                    this.DPSAIUnit.moveToCell(cell);
+                }
+            }else{//can hit
+                Cell cell = this.cellAtSmalestDistance(cells,this.DPSAIUnit);
+                this.DPSAIUnit.moveToCell(cell);
+                abl.AffectTarget(target);
+            }
+        }else{
+            return;//doesnt have mana
+        }
+        return;//doesnt have a single ability with mana for.
+    }
 
     @Override
     public void DefensiveBehavior() {
-
+        if(this.CurrentGame.getCurrentDifficulty().equals(Difficulty.DifficultyStage.EASY)){
+            return;
+        }else{
+            if(this.DPSAIUnit.getCurrentHPPercent()>=35){
+                return ;
+            }
+        }
+        if(this.DPSAIUnit.getRemainingMovement()==0){
+            return;
+        }
+        //Do The Shit
     }
 
     @Override
