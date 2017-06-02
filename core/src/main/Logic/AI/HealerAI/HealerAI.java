b7c5ba7364822ac0d3a319d74fa65542b47a2ff6
Tiago Magalhães <up201607931@fe.up.pt>
2017-06-02 19:45:21 +0100

    refactoring to move methods to a shared abstract class

--- core/src/main/Logic/AI/HealerAI/HealerAI.java
+++ core/src/main/Logic/AI/HealerAI/HealerAI.java
@@ -2,7 +2,15 @@ package Logic.AI.HealerAI;
 
 
 import Logic.AI.TankAI.TankAI;
+import Logic.GameController;
+import main.Logic.Unit.Unit;
 
 public class HealerAI{
+    private Unit HealerAIUnit = null;
+    private GameController CurrentGame = null;
 
+    public HealerAI(Unit owner,GameController game){
+        this.HealerAIUnit = owner;
+        this.CurrentGame = game;
+    }
 }
