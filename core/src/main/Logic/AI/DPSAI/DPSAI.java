4f222b6e2493bfc3a14db0b17f448e6dd9980f91
Tiago Magalhães <up201607931@fe.up.pt>
2017-06-02 20:08:31 +0100

    some things

--- core/src/main/Logic/AI/DPSAI/DPSAI.java
+++ core/src/main/Logic/AI/DPSAI/DPSAI.java
@@ -1,5 +1,40 @@
 package Logic.AI.DPSAI;
 
-public class DPSAI {
+import Logic.AI.BaseAIFeatures;
+import Logic.AI.BaseAi;
+import Logic.Difficulty;
+import Logic.GameController;
+import main.Logic.Unit.Unit;
 
+public class DPSAI extends BaseAIFeatures implements BaseAi{
+
+    @Override
+    public void DefensiveBehavior() {
+
+    }
+
+    @Override
+    public void OffensiveBehavior() {
+
+    }
+
+    @Override
+    public void FullTurnBehavior() {
+
+    }
+
+    @Override
+    public Unit getControledUnit() {
+        return null;
+    }
+
+    @Override
+    public GameController getCurrentGameController() {
+        return null;
+    }
+
+    @Override
+    public Difficulty.DifficultyStage getDifficulty() {
+        return null;
+    }
 }
--- core/src/main/Logic/AI/HealerAI/HealerAI.java
+++ core/src/main/Logic/AI/HealerAI/HealerAI.java
@@ -1,16 +1,49 @@
 package Logic.AI.HealerAI;
 
 
+import Logic.AI.BaseAIFeatures;
+import Logic.AI.BaseAi;
 import Logic.AI.TankAI.TankAI;
+import Logic.Difficulty;
 import Logic.GameController;
 import main.Logic.Unit.Unit;
 
-public class HealerAI{
+public class HealerAI extends BaseAIFeatures implements BaseAi{
     private Unit HealerAIUnit = null;
     private GameController CurrentGame = null;
 
     public HealerAI(Unit owner,GameController game){
         this.HealerAIUnit = owner;
         this.CurrentGame = game;
+    }
+
+    @Override
+    public void DefensiveBehavior() {
+        
+    }
+
+    @Override
+    public void OffensiveBehavior() {
+
+    }
+
+    @Override
+    public void FullTurnBehavior() {
+
+    }
+
+    @Override
+    public Unit getControledUnit() {
+        return null;
+    }
+
+    @Override
+    public GameController getCurrentGameController() {
+        return null;
+    }
+
+    @Override
+    public Difficulty.DifficultyStage getDifficulty() {
+        return null;
     }
 }
