a2cf86c65cf4753ff1d52ddfc9b0201228926faa
Diogo <up201505472@fe.up.pt>
2017-06-05 02:28:03 +0100

    Working facebook

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,6 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+
 import main.game.MyGdxGame;
 
 public class DesktopLauncher {
@@ -16,5 +21,12 @@ public class DesktopLauncher {
 		config.foregroundFPS = 0;
 		config.backgroundFPS = 0;
 		new LwjglApplication(new MyGdxGame(), config);
-	}
+
+        String acessToken = "EAACEdEose0cBAPwrunsZCS86lueQywK0gfc9rSgnqOCJS8MZBhVhVKVM7yZBs4eKg3V3Ug5csKyN94lNZAWeQ0LsUDIFT82qnJUyMrZB3Jp7wZC5Av6ridTG4TsW6sVkZCOZBMTYyU4QRun7VQoAojHZCAKBL6WwOBpcgjoIBCftG8vwiZB0cf5ISU8LY6j14M4bcZD";
+
+        FacebookClient fbClient = new DefaultFacebookClient(acessToken);
+
+        FacebookType response = fbClient.publish("me/feed", FacebookType.class, Parameter.with("message",  "Currently playing LPOOP game"), Parameter.with("link", "http://www.staggeringbeauty.com/"));
+
+    }
 }
