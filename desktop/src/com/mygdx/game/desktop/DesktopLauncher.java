f65c3adc1c61e1239f06130154b31e42965dcea3
Diogo <up201505472@fe.up.pt>
2017-06-05 03:15:01 +0100

    Working facebook

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -22,7 +22,7 @@ public class DesktopLauncher {
 		config.backgroundFPS = 0;
 		new LwjglApplication(new MyGdxGame(), config);
 
-        String acessToken = "EAACEdEose0cBAPwrunsZCS86lueQywK0gfc9rSgnqOCJS8MZBhVhVKVM7yZBs4eKg3V3Ug5csKyN94lNZAWeQ0LsUDIFT82qnJUyMrZB3Jp7wZC5Av6ridTG4TsW6sVkZCOZBMTYyU4QRun7VQoAojHZCAKBL6WwOBpcgjoIBCftG8vwiZB0cf5ISU8LY6j14M4bcZD";
+        String acessToken = "EAACEdEose0cBAC2nOV1MM8NVljMMurGZC1rhQvjLoaZCDULqPz3ZCJw3j0Ni2h8KjEqSMlHZCCAWDRWnq1ZBhO2bhuJOEdkNkcvqCwdFyembLyNiEhNbMkpUQ755ELYFAqWZCZC2YsY4uDfvACfI6c8bDsNdr6PQMrpO8Rp3cTOOqBC1wRdokmnHRp1EZB6RCUsZD";
 
         FacebookClient fbClient = new DefaultFacebookClient(acessToken);//
 
